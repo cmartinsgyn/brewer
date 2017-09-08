8659cea7cd222f59f8abe336294a5551e6e8eeb6
claudio <cmartinsgyn@gmail.com>
2017-09-07 23:40:23 -0300

    Introducao a Validacao

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,8 +1,28 @@
 package com.algaworks.brewer.controller;
 
+import com.algaworks.brewer.model.Cerveja;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 public class CervejasController {
 
+    @RequestMapping("/cervejas/novo")
+    public String novo() {
+        return "cerveja/CadastroCerveja";
+    }
+
+    @RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+    public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+        if (result.hasErrors()) {
+            System.out.println(">>>> tem erro sim!");
+        }
+
+        System.out.println(">>> sku: " + cerveja.getSku());
+        return "redirect:/cervejas/novo";
+    }
 }
